97ad57d65ae73107187e5334b0f3746cd48b552b
Bogdan Tielnyi <bogdan.tlny@gmail.com>
2025-06-04 18:11:09 +0300

    background changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,18 @@
 import Picture from "./components/Picture/Picture";
-import Text from "./components/Text/Text";
 import "./clock.css";
 import "./window.css";
 import "./desktop.css";
 export default function App() {
   return (
     <>
-      <Picture id={"dial"} src={"dial.svg"} width={100} className={"clock"}>
-        <Picture id={"arrow_big"} src={"arrow.svg"} width={100}/>
-        <Picture id={"arrow"} src={"arrow.svg"} width={100}/>
+      <Picture id={"dial"} src={"dial.svg"} width={150} className={"clock"}>
+        <Picture id={"arrow_big"} src={"arrow.svg"} width={150}/>
+        <Picture id={"arrow"} src={"arrow.svg"} width={150}/>
       </Picture>
       <Picture id={"window"} src={"window.svg"} width={200} className={"outside"}>
         <Picture id={"window_ground"} src={"window_ground.svg"} width={200}></Picture>
         <Picture id={"bush"} src={"bush.svg"} width={200}></Picture>
       </Picture>
-      <Text>
-        COMING SOON
-      </Text>
       <Picture id={"table"} src={"table.svg"} width={320} className={"desktop"}>
         <Picture id={"laptop"} src={"laptop.svg"} width={150}/>
         <Picture id={"calculator"} src={"calculator.svg"} width={60}/>
--- src/components/Text/Text.jsx
+++
@@ -1,8 +0,0 @@
-import "./Text.css";
-export default function Text({children}) {
-    return (
-        <div className="text">
-            {children}
-        </div>
-    );
-}
