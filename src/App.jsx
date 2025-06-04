82beb027710c5e5fbf3bfbcbfbee1d28c1580528
Bogdan Tielnyi <bogdan.tlny@gmail.com>
2025-06-04 16:07:49 +0300

    added 2d arrow to clock

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import Picture from "./components/Picture/Picture";
-
+import "./clock.css";
+import "./window.css";
 export default function App() {
   return (
     <>
-      <Picture id={"dial"} src={"dial.svg"} width={100}>
+      <Picture id={"dial"} src={"dial.svg"} width={100} className={"clock"}>
+        <Picture id={"arrow_big"} src={"arrow.svg"} width={100}/>
         <Picture id={"arrow"} src={"arrow.svg"} width={100}/>
       </Picture>
+      <Picture id={"window"} src={"window.svg"} width={200} className={"outside"}>
+        <Picture id={"window_ground"} src={"window_ground.svg"} width={200}></Picture>
+        <Picture id={"bush"} src={"bush.svg"} width={200}></Picture>
+      </Picture>
     </>
-  );
+  );  
 }
